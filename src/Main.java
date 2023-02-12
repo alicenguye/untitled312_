478c274428686f02476dc8deccf68a64f03d8bf5
Alice Nguyen <nguye4hn@mail.uc.edu>
2023-02-12 16:16:21 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        final int DRIVING_AGE = 18;
+        int userAge = 19;
+
+        if (userAge >= DRIVING_AGE)
+        {
+            System.out.println( "you can drive ");
+        }
+    }
+}
